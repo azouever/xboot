89ccef891f376afcf2bfc3249b42b45ffcdb7d36
kaixuan.xu <kaixuan.xu@haoji-hr.com>
2019-09-09 16:18:28 +0800

    fixed

--- src/test/com/process/boot/StringUtilsTest.java
+++ src/test/com/process/boot/StringUtilsTest.java
@@ -11,8 +11,18 @@ public class StringUtilsTest {
 
   @Test
   public void test0() {
-    String[] names = {"xukaixuan", "yangjie","liyadong"};
+    String[] names = {"xukaixuan", "yangjie", "liyadong"};
     String str = StringUtils.arrayToCommaDelimitedString(names);
     System.out.println(str);
+  }
+
+  @Test
+  public void test1() {
+    Boolean isOver = true;
+    System.out.println(isOver);
+    System.out.println(isOver.hashCode());
+    isOver = !isOver;
+    System.out.println(isOver);
+    System.out.println(isOver.hashCode());
   }
 }
