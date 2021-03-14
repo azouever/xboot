d4444425db2f32e4f3a6310d8041d6a5565f2c4c
kaixuan.xu <kaixuan.xu@ly.com>
2021-03-14 13:20:45 +0800

    fileSystemResources ioUtils copy stream

--- src/main/java/com/process/xboot/excel/FileController.java
+++ src/main/java/com/process/xboot/excel/FileController.java
@@ -1,12 +1,16 @@
 package com.process.xboot.excel;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -48,6 +52,34 @@ public class FileController {
 
       return new HttpEntity<>(new ByteArrayResource(inputStream.readAllBytes()), header);
     }
+  }
+
+
+  @GetMapping(value = "/anotherDown", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+  public FileSystemResource anotherDown() {
+    File file = new File(Thread.currentThread().getContextClassLoader()
+        .getResource("template/excel_template.xlsx").getFile());
+    return new FileSystemResource(file);
+  }
+
+  @GetMapping(value = "/anotherDownByCopyStream", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+  public void anotherDownByCopyStream(HttpServletResponse response) {
+
+    String templateName = "excel_template.xlsx";
+    try (InputStream inputStream = Thread.currentThread().getContextClassLoader()
+        .getResourceAsStream("template/" + templateName);
+    ) {
+      // copy it to response's OutputStream
+      response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+          "attachment;filename=" + URLEncoder.encode(templateName, "utf8"));
+      // 指明response的返回对象是文件流
+      org.apache.commons.io.IOUtils.copy(inputStream, response.getOutputStream());
+      response.flushBuffer();
+    } catch (IOException ex) {
+      log.info("Error writing file to output stream. Filename was '{}'", templateName, ex);
+      throw new RuntimeException("IOError writing file to output stream");
+    }
+
   }
 }
 
